be1d282bfec800a1738d2294ddaf473a582f09bb
nytrobound <nytrobound@gmail.com>
2017-06-05 18:51:24 +0200

    Added Ubuntu PPA to scripts and removed Lisk

--- installssl.sh
+++ installssl.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#This script will help you to generate a trusted SSL certificate issued by letsencrypt
-#This script uses https://github.com/certbot/certbot
-
-
-#Don't forget to vote for mrgr delegate in Lisk: 3125853987625788223L
-#Don't forget to vote for mrgr delegate in Shift: 14156057994616021440S
+# This script will help you to generate a trusted SSL certificate issued by Let's Encrypt using certbot.
 
 SSLUSER=$USER
 LOCAL_HOME=$(pwd)
@@ -18,13 +13,13 @@ echo "Installation start.." >> $LOG
 echo
 
 if [ -f "config.sh" ]; then
-	echo "**A previous installation was detected.." | tee -a $LOG
+	echo "Oh No! A previous installation was detected." | tee -a $LOG
 	echo "This script is meant to be executed only once." | tee -a $LOG
-	echo "If you are trying to run it again is because you possibly had a problem, if so please contact mrgr in https://lisk.chat/direct/mrgr or in https://shiftnrg.slack.com/messages/@mrgr/" | tee -a $LOG
+	echo "Exiting..." | tee -a $LOG
 	exit 0
 fi
 
-echo "This script will help you to get a new certificate from letsencrypt. Please follow the instructions."
+echo "This script will help you to generate a trusted SSL certificate issued by Let's Encrypt using certbot."
 echo
 echo -n "Enter your domain name: "
 	read DOMAIN_NAME
@@ -33,10 +28,9 @@ echo -n "Enter your email: "
 echo -n "Enter the port you will use for HTTPS: "
         read HTTPS_PORT
 echo
-echo "Now this script need something from you."
-echo -e "Before proceeding please check your ufw configuration, execute in a new terminal: ${CYAN}sudo ufw status${OFF}"
-echo "You need to have your own ports enable, obligatory your SSH port and your LISK client port."
-echo -e "Execute ${CYAN}ifconfig${OFF} in the other terminal and look for network interface (normally is eth0, eth1, eth2, ens1, ens2, ens3...)."
+echo -e "Before proceeding, please check your ufw configuration. To do this, execute ${CYAN}sudo ufw status${OFF} in a new terminal on your server."
+echo "You'll need to have your own ports enable, especially your SSH port and your SHIFT client port."
+echo -e "Execute ${CYAN}ifconfig${OFF} in the other terminal and look for the network interface of your server (normally is eth0, eth1, eth2, ens1, ens2, ens3...)."
 echo -n "What is your network interface?: "
         read NETWORK_INTERFACE
 echo
@@ -48,28 +42,33 @@ echo "Your network interface is: $NETWORK_INTERFACE" >> $LOG
 echo
 echo -n "Enabling port 443/tcp.. " | tee -a $LOG
 sudo ufw allow 443/tcp &>> $LOG || { echo "Could not enable port 443. Please read your logs/installssl.log file. Exiting."  | tee -a $LOG && exit 1; }
+echo "Done."
+echo
+echo -n "Backing up firewall.." | tee -a $LOG
 sudo cp /etc/ufw/before.rules before.rules.backup #Backing up file /etc/ufw/before.rules
-echo "done" | tee -a $LOG
+echo "Done." | tee -a $LOG
 
 echo
 echo -n "Installing certbot.. " | tee -a $LOG
-cd /opt
-sudo git clone https://github.com/certbot/certbot.git &>> $LOG || { echo "Could not clone git certbot source. Please read your logs/installssl.log file. Exiting." | tee -a $LOG && exit 1; }
-echo "done" | tee -a $LOG
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
+echo "Done." | tee -a $LOG
 export LC_ALL="en_US.UTF-8" >> $LOG
 export LC_CTYPE="en_US.UTF-8" >> $LOG
 
 echo
 echo -n "Generating new SSL certificate.. this could take some minutes.." | tee -a $LOG
-/opt/certbot/certbot-auto certonly --standalone -d $DOMAIN_NAME --email $EMAIL --agree-tos --non-interactive &>> $LOG || { echo "Could not generate SSL certificate. Please read your logs/installssl.log file. Exiting." | tee -a $LOG && exit 1; }
-echo "done" | tee -a $LOG
+certbot certonly --standalone -d $DOMAIN_NAME --email $EMAIL --agree-tos --non-interactive &>> $LOG || { echo "Could not generate SSL certificate. Please read your logs/installssl.log file. Exiting." | tee -a $LOG && exit 1; }
+echo "Done." | tee -a $LOG
 sudo chmod 755 /etc/letsencrypt/archive/
 sudo chmod 755 /etc/letsencrypt/live/
 
 echo
 echo -n "Installing redirection on port 443 to port $HTTPS_PORT.. " | tee -a $LOG
 sudo sh -c "echo >> /etc/ufw/before.rules" >> $LOG
-sudo sh -c "echo \"# MRGR -- Auto-Redirect \" >> /etc/ufw/before.rules" >> $LOG
+sudo sh -c "echo \"# free-ssl -- Auto-Redirect \" >> /etc/ufw/before.rules" >> $LOG
 sudo sh -c "echo \"*nat\" >> /etc/ufw/before.rules" >> $LOG
 sudo sh -c "echo \":PREROUTING ACCEPT [0:0]\" >> /etc/ufw/before.rules" >> $LOG
 sudo sh -c "echo \"-A PREROUTING -i $NETWORK_INTERFACE -p tcp --dport 443 -j REDIRECT --to-port $HTTPS_PORT\" >> /etc/ufw/before.rules" >> $LOG
@@ -84,18 +83,17 @@ echo "HTTPS_PORT=\"$HTTPS_PORT\"" >> $CONF
 echo "NETWORK_INTERFACE=\"$NETWORK_INTERFACE\"" >> $CONF
 
 echo
-echo "*************************************** Please do the following:"
+echo "Please do the following"
 echo -e "Run: ${CYAN}sudo nano /etc/ufw/before.rules${OFF}"
-echo "And go to the bottom of the file and check your last 4 lines, should be like follow:"
+echo "Go to the bottom of the file and check your last 4 lines, that should look like this:"
 echo "    *nat"
 echo "    :PREROUTING ACCEPT [0:0]"
 echo "    -A PREROUTING -i $NETWORK_INTERFACE -p tcp --dport 443 -j REDIRECT --to-port $HTTPS_PORT"
 echo "    COMMIT"
 echo
-echo "Please confirm the above. If everything is right this script needs to reload your firewall."
-echo "* Be aware that if the /etc/ufw/before.rules file contains other type of lines at the end of the file your firewall might not work properly."
-echo "* If you are seeing other type of lines don't worry, this script has backed up your file and you can find it here: $LOCAL_HOME/before.rules.backup"
-echo "* If you don't accept the following question this script will restore your file to the above version."
+echo "* If everything is correct, confirm with y and the script will reload your firewall."
+echo "* If there is something else or wrong, confirm with n and the script will restore a backup."
+echo "* Be aware that if the /etc/ufw/before.rules file contains other type of lines at the end of the file, your firewall might not work properly."
 
 	read -p "Do you want to continue (y/n)?: " -n 1 -r
 	if [[  $REPLY =~ ^[Yy]$ ]]
@@ -125,10 +123,10 @@ echo "* If you don't accept the following question this script will restore your
 
 echo
 echo
-echo "++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG
-echo "Your SSL Certificate has been created successfully, now you need to perform the following manual task." | tee -a $LOG
+echo " Your SSL Certificate has been created successfully, now you need to perform the following manual task: " | tee -a $LOG
+echo "########################################################################################################" | tee -a $LOG
 echo
-echo "Go to your Lisk config.json file and edit ssl section like the following:" | tee -a $LOG
+echo "Go to your Shift config.json file and edit the ssl section like the following:" | tee -a $LOG
 echo "    \"ssl\": {" | tee -a $LOG
 echo -e "        \"enabled\": ${CYAN}true${OFF},"
 echo "        \"enabled\": true," >> $LOG
@@ -144,22 +142,17 @@ echo "        }" | tee -a $LOG
 echo "    }," | tee -a $LOG
 
 echo
-echo "Save and exit from your config.json file." | tee -a $LOG
-echo "For Lisk perform : bash lisk.sh reload" | tee -a $LOG
-#echo "For Shift perform: ./shift_manager.bash stop && ./shift_manager.bash start" | tee -a $LOG
-#echo "                   or simply stop and start your node app.js" | tee -a $LOG
+echo "Please save your config.json file and reload Shift with ./shift_manager.bash reload" 
 echo
-echo -e "${CYAN}Installation Successfully Completed${OFF}"
-echo "Installation Successfully Completed" >> $LOG
+echo "****************************************************"
+echo "* ${CYAN}Installation Successfully Completed${OFF} *" | tee -a $LOG
+echo "****************************************************"
 echo
-echo "Now you can visit your address https://$DOMAIN_NAME and see the result. :)" | tee -a $LOG
+echo "You can now visit your address https://$DOMAIN_NAME and confirm the result." | tee -a $LOG
 echo " "  | tee -a $LOG
-echo "Now to enable the renewssh.sh script you need to add a new cron task." | tee -a $LOG
-echo "It is recommended to use http://www.crontab-generator.org/ to help you with your expression" | tee -a $LOG
-echo -e "Example for check your SSL certificate every Wednesday at 12pm you need to run ${CYAN}sudo crontab -e${OFF} and add at the end:"
-echo "Example for check your SSL certificate every Wednesday at 12pm you need to run sudo crontab -e and add at the end:" >> $LOG
+echo "Now you'll need to add a cronjob to renew your certificate regularly." | tee -a $LOG
+echo "It is recommended to use http://www.crontab-generator.org/ to help you with your cronjob." | tee -a $LOG
+echo "Example: To check and renew your SSL certificate every Wednesday at 12pm you need to run sudo crontab -e and add at the end:" | tee -a $LOG
 echo "* 12 * * WED bash /home/$SSLUSER/free-ssl/start_renew.sh >> /home/$SSLUSER/free-ssl/logs/cron.log" | tee -a $LOG
-#echo "To renew a certificate in Shift node please add at the end: /home/$SSLUSER/free-ssl/start_renew.sh shift" | tee -a $LOG
 echo " " | tee -a $LOG
-echo "Don't forget to vote for mrgr delegate." | tee -a $LOG
 
--- renewssl.sh
+++ renewssl.sh
@@ -10,7 +10,7 @@ LOG=/home/$SSLUSER/free-ssl/logs/renewssl.log
 RENEW="0"
 TIME=$(date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S")
 cert="/etc/letsencrypt/live/$DOMAIN/cert.pem"
-echo "############ Checking expiring date on cert $cert" >> $LOG
+echo "Checking expiring date on cert $cert.." >> $LOG
 if openssl x509 -checkend 86400 -noout -in $cert
 then
   echo "The certificate is good." >> $LOG
@@ -24,36 +24,38 @@ then
   echo "Days left : $daysleft" >> $LOG
   echo " " >> $LOG
   if [ "$daysleft" -lt "30" ]; then
-        echo "Less than 30 days.. it will start the renewal script.." >> $LOG
+        echo "Less than 30 days.. execute renewal script.." >> $LOG
         RENEW="1"
   else
-        echo "Greater than 30 days, no need to renew." >> $LOG
+        echo "Greater than 30 days, no need for renewal." >> $LOG
   fi
 else
   echo "This certificate does not exist or could not be successfully renewed. You can do the following:" >> $LOG
-  echo " - Check if your domain is still alive." >> $LOG
-  echo " - Check if the folder /etc/letsencrypt/live/$DOMAIN/ exists and it contain *.pem files." >> $LOG
-  echo " - If nothing of the above works, remove /etc/letsencrypt/live/$DOMAIN/ and run the installer again: bash installssl.sh" >> $LOG
+  echo " * Check if your domain is still alive." >> $LOG
+  echo " * Check if the folder /etc/letsencrypt/live/$DOMAIN/ exists and it contain *.pem files." >> $LOG
+  echo " * If nothing of the above works, remove /etc/letsencrypt/live/$DOMAIN/ and run the installer again: bash installssl.sh" >> $LOG
 fi
-echo "############" >> $LOG
 echo " " >> $LOG
 
 if [ "$RENEW" -eq "1" ]; then
-echo "+++++++++++++++++++++++" >> $LOG
-echo "Starting renew script" >> $LOG
+echo 
+echo "Starting renew script.." >> $LOG
+echo "#######################"
 echo "$TIME" >> $LOG
 echo "Removing port redirect.." >> $LOG
+echo "************************"
 sudo head -n -4 /etc/ufw/before.rules > before.rules
 sudo rm /etc/ufw/before.rules >> $LOG
 sudo cp before.rules /etc/ufw/before.rules >> $LOG
-echo "done" >> $LOG
+echo "Done." >> $LOG
 echo "Running certbot-auto renew.." >> $LOG
 sudo ufw allow 443/tcp &> /dev/null
-sudo /opt/certbot/certbot-auto renew --agree-tos >> $LOG || { echo "Could not generate SSL certificate in renewssl.sh. Please read your logs/renewssl.log file. Exiting." | tee -a $LOG && exit 1; }
+sudo certbot renew --agree-tos >> $LOG || { echo "Could not generate SSL certificate in renewssl.sh. Please read your logs/renewssl.log file. Exiting." | tee -a $LOG && exit 1; }
 sudo ufw delete allow 443/tcp &> /dev/null
 sudo ufw reload >> $LOG
-echo "done" >> $LOG
+echo "Done." >> $LOG
 echo -n "Installing port redirect.." >> $LOG
+echo "**************************"
 echo "*nat" >> before.rules
 echo ":PREROUTING ACCEPT [0:0]" >> before.rules
 echo "-A PREROUTING -i $NETWORK_INTERFACE -p tcp --dport 443 -j REDIRECT --to-port $HTTPS_PORT" >> before.rules
@@ -66,19 +68,12 @@ echo "done" >> $LOG
 echo "Reload firewall.." >> $LOG
 sudo ufw reload >> $LOG
 echo "done" >> $LOG
-if [ "$1" -eq "shift" ]; then
-   echo "Stop and start Shift to take the new certificate" >> $LOG
-   SHIFT_SCREEN="shift"
-   screen -S $SHIFT_SCREEN -p 0 -X stuff "^C"
-   screen -S $SHIFT_SCREEN -p 0 -X stuff "node app.js$(printf \\r)"
-   echo "done" >> $LOG
-else
-   echo "Reloading Lisk to take the new certificate" >> $LOG
-   bash /home/$SSLUSER/lisk-main/lisk.sh reload >> $LOG
-   echo "done" >> $LOG
-fi
+   echo "Reload Shift to take the new certificate" >> $LOG
+   bash /home/$SSLUSER/shift/shift_manager.bash reload >> $LOG
+   echo "Done." >> $LOG
 TIME=$(date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S")
-echo "Certificate renewal completed" >> $LOG
+echo "*********************************"
+echo "* Certificate renewal completed *" >> $LOG
+echo "*********************************"
 echo "$TIME" >> $LOG
-fi
 
